ab832970f0e00bb7fbf4bca1c6d3500f9b807de4
AndreFitara <100498056+AndreFitara@users.noreply.github.com>
2025-05-11 19:32:24 +0700

    Create BangunDatar.java

---
+++ PRAKTIKUM 9/Tugas 3.4/BangunDatar.java
@@ -0,0 +1,11 @@
+/**
+* File      : BangunDatar.java
+* Deskripsi : kelas abstrak untuk bangun datar
+* Pembuat   : Andre Fitara Agun Ni'am Muta'ali
+* Tanggal   : 08/05/2025
+*/
+
+public abstract class BangunDatar {
+    public abstract double hitungKeliling();
+    public abstract double hitungLuas();
+}
